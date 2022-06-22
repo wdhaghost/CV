5338990c8b699a98c481611422e30718b2e7dbca
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-22 15:57:26 +0200

    JS working in mobile version

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById("profil").addEventListener("click",function(event){
+    document.getElementById("info").classList.toggle("active")
+})
+
+document.getElementById("skill-btn").addEventListener("click",function(event){
+    document.getElementById("skills").classList.toggle("active")
+})
+document.getElementById("exp-btn").addEventListener("click",function(event){
+    document.getElementById("exp").classList.toggle("active")
+})
+
+document.getElementById("degree-btn").addEventListener("click",function(event){
+    document.getElementById("degree").classList.toggle("active")
+})
