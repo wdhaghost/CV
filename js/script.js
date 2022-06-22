aa9623a0abd1f3c3f99d5d3abaef17108867b0e4
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2022-06-22 17:03:47 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+let lastActive=0;
 
 document.getElementById("profil").addEventListener("click",function(event){
     document.getElementById("info").classList.toggle("active")
@@ -14,3 +14,29 @@ document.getElementById("exp-btn").addEventListener("click",function(event){
 document.getElementById("degree-btn").addEventListener("click",function(event){
     document.getElementById("degree").classList.toggle("active")
 })
+
+
+const navList=document.querySelectorAll(".nav-list .nav-item")
+for (const item of navList ) {
+   
+    item.addEventListener("click",function(event){
+        for (const i of navList) {
+            document.getElementById(i.dataset.name).classList.remove("active")
+            event.preventDefault()
+        }
+
+        document.getElementById(item.dataset.name).classList.add("active")
+    
+    })
+}
+
+window.addEventListener("resize", function(event){
+    if(window.innerWidth>=768){
+        for (const i of navList) {
+            document.getElementById(i.dataset.name).classList.remove("active")
+            event.preventDefault()
+        }
+        
+        document.getElementById("exp").classList.add("active")
+    }
+  })
